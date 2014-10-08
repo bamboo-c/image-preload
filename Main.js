44bf9a457ea2037c48ef3959aad3decccddb6ad3
Komei Otake <komei1220@gmail.com>
2014-10-08 18:57:21 +0900

    complete func update and rename for check

--- Main.js
+++ Main.js
@@ -2,12 +2,19 @@ $( function(){ Main.init(); } );
 
 var Main = {
 
+  imagePreload;
+
   //-------------------------------------------------
   // initialize
   //-------------------------------------------------
   init : function() {
 
     Main.imagePreload = new ImagePreload();
+
+  },
+  bar : function() {
+
+    console.log("preload done!");
 
   }
 
